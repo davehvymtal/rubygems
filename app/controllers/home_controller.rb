fe1eb6c1822df8b0ca7db055a7bbe22ced2a6588
David Cardenas <david_evil_64@hotmail.com>
2020-07-12 07:11:28 +0000

    Se realiza creacion de index page, mejoras visuales de los cursos

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,10 @@
 class HomeController < ApplicationController
+  #si el usuario no esta autenticado redirige al index
+  skip_before_action :authenticate_user!, :only => [:index]
   def index
+    #se crea variable courses para el home index con todos los cursos
+    @courses = Course.all.limit(3)
+    #se crea variable para los ultimos 3 cursos creados por fecha
+    @latest_courses = Course.all.limit(3).order(created_at: :desc)
   end
 end
